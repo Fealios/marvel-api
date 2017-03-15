c68cb596e30c76dece5b85909fe4a65ff9621321
Melvin Gruschow and Nicholas Mead <melvin+nicholas@>
2017-03-15 11:14:00 -0700

    working in jquery?

--- js/marvel-interface.js
+++ js/marvel-interface.js
@@ -1,4 +1,9 @@
 var Marvel = require("./../js/marvel.js").marvel;
+var marvelApi = require("marvel-api");
+var marvel = marvelApi.createClient({
+  publicKey: '8840f5d9676b5998d8bb0b4dd63ba1ea'
+, privateKey: 'd1c920ff3e7fcff7cdbf84b6089053dfd9ce29bd'
+});
 var heroName;
 
 $(document).ready(function(){
@@ -6,7 +11,16 @@ $(document).ready(function(){
     event.preventDefault();
     heroName = $('#name').val();
 
-    newHero = new Marvel(heroName);
-    newHero.getFirst20Issues(heroName);
+    marvel.characters.findByName(heroName)
+      .then(function(res) {
+        console.log('Found character ID', res.data[0].id);
+        return marvel.characters.comics(res.data[0].id);
+      })
+      .then(function(res) {
+        console.log(res.meta.count, res.meta.total);
+        console.log(res.data);
+      })
+      .fail(console.error)
+      .done();
   })
 })
